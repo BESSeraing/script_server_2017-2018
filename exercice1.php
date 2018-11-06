5d8df80ea2c1e7847fc6584ed531c2961f7ce43d
kokmok <jona303@gmail.com>
2018-11-06 16:56:42 +0100

    first lesson arrays

---
+++ exercice1.php
@@ -0,0 +1,16 @@
+<?php
+
+//PArcours ce tableau pour écrire l'ensemble des prénoms des élèves
+$studentFirstNames = ["Eric","Marco","Sébastien","Laura"];
+
+
+//Parcours ce tableau pour écrire l'ensemble des noms de famille des élèves.
+$students = [
+    ['firstName'=>"Eric",'lastName'=>"Gony"],
+    ['firstName'=>"Marco",'lastName'=>"SCIMECA"],
+    ['firstName'=>"Sébastien",'lastName'=>"MARCIPONT"],
+    ['firstName'=>"Laura",'lastName'=>"KUMAR"],
+];
+
+
+
---
+++ script.php
@@ -0,0 +1,44 @@
+<?php
+
+$name = "Jona";
+$age = 38;
+$isStudent = false;
+
+
+//echo $name.' '.$age;
+
+//Loops
+$fruits = ["pomme","poire","fraise"];
+$i =0;
+while($i<count($fruits)){
+    echo $fruits[$i].'<br>';
+    $i++;
+}
+for($n=0;$n<count($fruits);$n++){
+    echo $fruits[$n].'<br>';
+}
+
+foreach ($fruits as $fruit){
+    echo $fruit.'<br>';
+}
+
+//Tableaux associatifs
+$vCard = [
+    'email'=>"jonathan.cambier@gmail.com",
+    'lastName'=>'Cambier',
+    'firstName'=>"Jonathan"
+];
+
+foreach ($vCard as $vCardElement){
+    echo $vCardElement.'<br>';
+}
+
+foreach ($vCard as $key => $vCardElement){
+    echo $key .' : '.$vCardElement.'<br>';
+}
+
+print_r($vCard);
+
+var_dump($vCard);
+
+
