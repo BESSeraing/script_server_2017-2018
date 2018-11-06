00bd754a2ac5d08378a9085a22f70eb45ccd76d4
kokmok <jona303@gmail.com>
2018-11-06 15:04:37 +0100

    cleanage

--- xss/index.php
+++
@@ -1,7 +0,0 @@
-<form method="post" action=""><input type="text" name="toto"><input type="submit"></form>
-
-<?php
-
-$toto = filter_input(INPUT_POST,"toto",FILTER_SANITIZE_STRING);
-
-print_r($toto);
