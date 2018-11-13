9653473984a4cfeded88345d8263599e065997b3
kokmok <jona303@gmail.com>
2018-11-13 15:56:14 +0100

    exerice GET

--- exercice1.php
+++ exercice1.php
@@ -6,11 +6,18 @@ $studentFirstNames = ["Eric","Marco","Sébastien","Laura"];
 
 //Parcours ce tableau pour écrire l'ensemble des noms de famille des élèves.
 $students = [
-    ['firstName'=>"Eric",'lastName'=>"Gony"],
+    ['firstName'=>"Eric",'lastName'=>"Gony"], 
     ['firstName'=>"Marco",'lastName'=>"SCIMECA"],
     ['firstName'=>"Sébastien",'lastName'=>"MARCIPONT"],
     ['firstName'=>"Laura",'lastName'=>"KUMAR"],
 ];
 
+//var_dump($students);
+
+echo '<hr>';
+//echo $students[2]["lastName"];
 
 
+foreach ($students as $student){
+    var_dump($student['lastName']);
+}
---
+++ get.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$menu = [
+    ['name'=>'Accueil',"content"=>"Le contenu de la homePage"],
+    ['name'=>'About',"content"=>"Ceci est le contenu sur moi-même"],
+    ['name'=>'Contact',"content"=>"Ceci est sensé être un formulaire de contact"],
+    ['name'=>'funny',"content"=>"LOL"]
+];
+
+
+
+
+
