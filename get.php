5843579063b436c8e024eda9441f259f716d5632
kokmok <jona303@gmail.com>
2018-11-20 15:35:21 +0100

    GET advanced solution

--- get.php
+++ get.php
@@ -7,6 +7,30 @@ $menu = [
 ];
 
 
+foreach ($menu as $menuElement){
+    echo '<li><a href="?page='.$menuElement['url'].'">'.$menuElement['name'].'</a></li>';
+}
+
+
+$pageUrl = 'home';
+if (isset($_GET['page'])){
+    $pageUrl = $_GET['page'];
+}
+$page = findPage($menu,$pageUrl);
+if ($page !== null){
+    echo '<h1>'.$page['name'].'</h1>';
+    echo '<p>'.$page['content'].'</p>';
+}
+
+
+function findPage($menu,$url){
+    foreach ($menu as $menuElement){
+        if ($menuElement['url'] === $url){
+            return $menuElement;
+        }
+    }
+    return null;
+}
 
 
 
